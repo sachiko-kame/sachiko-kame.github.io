217e3549112571ae1d47a65ea53c357a004ab65d
sachiko-kame <sachiko-kame@users.noreply.github.com>
2020-09-20 11:59:08 +0900

    反応よくして、アイコンもかえるようにして

--- sachiko.js
+++ sachiko.js
@@ -1,6 +1,15 @@
-$(function(){
-  $('#sachiko-main-img').click(function(){
-    $('[data-toggle="popover"]').popover()
-  });
+$(function() {
+    $('#sachiko-main-img').ready(function() {
+        $('[data-toggle="popover"]').popover();
+    });
 
+    $('#oldSachiko-icon').click(function() {
+      const oldSachikoImage = 'images/old_sachiko.png';
+      const nowSachikoImage = 'images/sachiko.png';
+        if ($('#sachiko-main-img').attr('src') == nowSachikoImage) {
+            $('#sachiko-main-img').attr('src', oldSachikoImage);
+        } else {
+            $('#sachiko-main-img').attr('src', nowSachikoImage);
+        }
+    });
 });
